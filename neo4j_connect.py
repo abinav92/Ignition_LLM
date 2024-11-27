fe818c5e450c681ca0d4a2d9fd0fe1f4e754e5d9
Abi <abinav92@gmail.com>
2024-11-28 00:09:12 +0800

    create neo4j db and establish connection

---
+++ neo4j_connect.py
@@ -0,0 +1,9 @@
+from neo4j import GraphDatabase
+
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+URI = "bolt://localhost:7687"
+AUTH = ("neo4j", "password")
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+    print("Connection established.")
